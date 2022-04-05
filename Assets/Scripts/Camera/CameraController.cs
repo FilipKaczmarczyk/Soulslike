c413000538747e1f7daab3fe57374968e3d6f2ee
HappyArroW <stifmeister0700@gmail.com>
2022-04-05 19:41:46 +0200

    Added camera colision detection

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,59 +1,93 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Camera
 {
     public class CameraController : MonoBehaviour
     {
-        public GameObject target;
-        public Transform camera;
-        public Transform cameraPivot;
-
-        private Vector3 _cameraTransformPosition;
-        public LayerMask ignoreLayers;
-
         public static CameraController cc;
-
+        
+        [SerializeField] private Transform defaultCamera;
+        [SerializeField] private Transform cameraPivot;
+        [SerializeField] private GameObject cameraTarget;
+        [SerializeField] private LayerMask ignoreLayers;
+        
         [SerializeField] private float lookSpeed = 0.1f;
         [SerializeField] private float followSpeed = 0.1f;
         [SerializeField] private float pivotSpeed;
         [SerializeField] private float minimumPivot = -35;
         [SerializeField] private float maximumPivot = 35;
+        
+        [SerializeField] private float cameraSphereRadius = 0.2f;
+        [SerializeField] private float collisionOffset = 0.2f;
+        [SerializeField] private float minimumCollisionOffset = 0.2f;
+        
+        private Vector3 _cameraTransformPosition;
+        private Vector3 _cameraFollowVelocity;
 
-        private float defaultPositon;
-        private float lookAngle;
-        private float pivotAngle;
+        private float _targetPosition;
+        private float _defaultPosition;
+        private float _lookAngle;
+        private float _pivotAngle;
 
         private void Awake()
         {
             cc = this;
-            defaultPositon = camera.transform.localPosition.z;
+            _defaultPosition = defaultCamera.transform.localPosition.z;
             ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
         }
 
         public void FollowTarget(float delta)
         {
-            var targetPosition = Vector3.Lerp(transform.position, target.transform.position, delta / followSpeed);
+            var targetPosition = Vector3.SmoothDamp(transform.position, cameraTarget.transform.position,
+                ref _cameraFollowVelocity, delta / followSpeed);
+            
             transform.position = targetPosition;
+            
+            HandleCameraCollision(delta);
         }
 
         public void HandleCameraRotation(float delta, float mouseXInput, float mouseYInput)
         {
-            lookAngle += (mouseXInput * lookSpeed) / delta;
+            _lookAngle += (mouseXInput * lookSpeed) / delta;
             
-            pivotAngle -= (mouseYInput * pivotSpeed) / delta;
-            pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+            _pivotAngle -= (mouseYInput * pivotSpeed) / delta;
+            _pivotAngle = Mathf.Clamp(_pivotAngle, minimumPivot, maximumPivot);
             
             var rotation = Vector3.zero;
-            rotation.y = lookAngle;
+            rotation.y = _lookAngle;
             var targetRotation = Quaternion.Euler(rotation);
             transform.rotation = targetRotation;
             
             rotation = Vector3.zero;
-            rotation.x = pivotAngle;
+            rotation.x = _pivotAngle;
             
             targetRotation = Quaternion.Euler(rotation);
             cameraPivot.localRotation = targetRotation;
+        }
+
+        private void HandleCameraCollision(float delta)
+        {
+            _targetPosition = _defaultPosition;
+            
+            var direction = _cameraTransformPosition - cameraPivot.position;
+            direction.Normalize();
+            
+            if (Physics.SphereCast(cameraPivot.transform.position, cameraSphereRadius, direction, out var hit, 
+                Mathf.Abs(_targetPosition), ignoreLayers))
+            {
+                var distance = Vector3.Distance(cameraPivot.transform.position, hit.point);
+                _targetPosition = -(distance - collisionOffset);
+            }
+
+            if (Mathf.Abs(_targetPosition) < minimumCollisionOffset)
+            {
+                _targetPosition = -minimumCollisionOffset;
+            }
+
+            _cameraTransformPosition.z = Mathf.Lerp(_cameraTransformPosition.z, _targetPosition, delta / 0.2f);
+            defaultCamera.localPosition = _cameraTransformPosition;
         }
     }
 }
